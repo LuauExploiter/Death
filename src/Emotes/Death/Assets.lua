48d73f53a14d0d86fd3d59b351d293c361399809
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 21:58:03 -0500

    Update Assets.lua

--- src/Emotes/Death/Assets.lua
+++ src/Emotes/Death/Assets.lua
@@ -1,13 +1,16 @@
 return {
+	Bundle = "BadWolf",
+
 	Startup = {
 		{ From = "BadWolf/HandleL", To = "Character/Left Arm" },
 		{ From = "BadWolf/HandleR", To = "Character/Right Arm" },
-		{ From = "BadWolf/Left", To = "Character/HumanoidRootPart" },
-		{ From = "BadWolf/Right", To = "Character/HumanoidRootPart" },
+		{ From = "BadWolf/Left", To = "Character/HumanoidRootPart", SetWolf = true },
+		{ From = "BadWolf/Right", To = "Character/HumanoidRootPart", SetWolf = true },
 	},
+
 	Optional = {
 		{ From = "BadWolf/Sparks2", To = "Character" },
-		{ From = "BadWolf/SpinL", To = "Character" },
-		{ From = "BadWolf/SpinR", To = "Character" },
+		{ From = "BadWolf/SpinL", To = "Character/Left Arm" },
+		{ From = "BadWolf/SpinR", To = "Character/Right Arm" },
 	},
 }

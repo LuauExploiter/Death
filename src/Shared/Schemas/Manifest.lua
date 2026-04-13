ad8c94bf2d309311ea67e69a3b062fbb59e32318
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:12:14 -0500

    Update Manifest.lua

--- src/Shared/Schemas/Manifest.lua
+++ src/Shared/Schemas/Manifest.lua
@@ -1,1 +1,12 @@
-return {}
+local ManifestSchema = {}
+
+function ManifestSchema.validate(manifest)
+	assert(type(manifest) == "table", "Manifest must be a table")
+	assert(type(manifest.Name) == "string", "Manifest.Name missing")
+	assert(type(manifest.Animation) == "string", "Manifest.Animation missing")
+	assert(type(manifest.Sounds) == "string", "Manifest.Sounds missing")
+	assert(type(manifest.VFX) == "string", "Manifest.VFX missing")
+	return true
+end
+
+return ManifestSchema

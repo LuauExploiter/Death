69187dab95f87647c10486fafc623bd706b7390b
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:13:52 -0500

    Update convert_kfs_to_animdata.lua

--- tools/convert_kfs_to_animdata.lua
+++ tools/convert_kfs_to_animdata.lua
@@ -1,1 +1,19 @@
-print("stub")
+local function convert(kfs)
+	local out = {
+		Loop = false,
+		Priority = Enum.AnimationPriority.Action,
+		Frames = {},
+	}
+
+	for _, keyframe in ipairs(kfs:GetKeyframes()) do
+		table.insert(out.Frames, {
+			Name = keyframe.Name,
+			Time = keyframe.Time,
+			Pose = {},
+		})
+	end
+
+	return out
+end
+
+return convert

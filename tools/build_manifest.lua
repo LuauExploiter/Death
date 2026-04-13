c9d1d1d9639d8536193077aa94988a27fb113579
LuauExploiter <beshoyibrahim283@gmail.com>
2026-04-12 22:14:04 -0500

    Implement build function in build_manifest.lua

--- tools/build_manifest.lua
+++ tools/build_manifest.lua
@@ -1,1 +1,13 @@
-print("stub")
+local function build(name, animationPath, soundsPath, vfxPath, cameraPath, cleanupPath, assetsPath)
+	return {
+		Name = name,
+		Animation = animationPath,
+		Sounds = soundsPath,
+		VFX = vfxPath,
+		Camera = cameraPath,
+		Cleanup = cleanupPath,
+		Assets = assetsPath,
+	}
+end
+
+return build
